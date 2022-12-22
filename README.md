a7e03443816a6910963cbdd26af30274c1561beb
stefan <stefan_paksa@icloud.com>
2022-12-22 12:29:07 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+## Test Exporter for Prometheus
+
+So let's assume we have a registration/authentication application. And we are system administrators who need to track 
+how many successful authentication or registration requests are received and how many 
+bad registration or authentication requests are received.
+For this task we decide that we will use Prometheus.
+
+#### How can we resolve this task with Prometheus?
+
+At first, we need to create metrics. 
+Then we need to send metrics on "/metrics" URL
+
+What kind of metrics will we have ?
+Our metrics has one field "request" with type counter vector 
+
+And "request" has two labels:\
+    _type - type of request(auth,reg)\
+    _response - type of response(ok, error)
+
+Then we send this metric on "/metrics" and write prometheus config.
+In config, we specify addr for scarping and interval for requests.
+
+Run prometheus with the config and looking at result.
+
+QUICK START with DOCKER 
+
+```
+$ docker run --name prometheus -d --network="host" prom/prometheus
+$ docker cp config/prometheus.yml prometheus:/etc/prometheus
+$ docker restart prometheus
+```
+
+
+#### PromQL commands:
+rate - different between request
+
+
+### GRAFANA and node-exporter:
+Setup Grafana: 
+```
+$ docker run --name grafa -d --network="host" grafana/grafana
+```
+login:password for grafana(port 3000):
+```
+admin:admin
+```
+Config for grafana dashboard:
+```
+config/node-exporter.json
+```
+Setup node-exporter:
+```
+git clone https://github.com/prometheus/node_exporter.git
+cd node_exporter
+make build
+./node_exporter
+```
---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+type Metrics struct {
+	requestTotal *prometheus.CounterVec
+}
+
+var metrics Metrics
+
+func createMetrics() {
+	metrics.requestTotal = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "test_app_requests",
+			Help: "Info about request: type(AUTH/REG), response(OK, ERROR)",
+		},
+		[]string{"type", "response"},
+	)
+	prometheus.MustRegister(metrics.requestTotal)
+}
+
+var RPS = 40
+
+var requestTypes = []string{"AUTH", "REG"}
+var typesProbs = []float64{.7, .3}
+
+var requestResponses = []string{"OK", "ERROR"}
+var responseProbs = []float64{.99, .01}
+
+func getRandom(values []string, probs []float64) string {
+	p := rand.Float64()
+
+	for i, value := range values {
+		if p < probs[i] {
+			return value
+		}
+		p -= probs[i]
+	}
+	return values[len(values)-1]
+}
+
+func getType() string {
+	return getRandom(requestTypes, typesProbs)
+}
+
+func getResponse() string {
+	return getRandom(requestResponses, responseProbs)
+}
+
+func performRequest() {
+	type_ := getType()
+	response_ := getResponse()
+	metrics.requestTotal.With(prometheus.Labels{
+		"type":     type_,
+		"response": response_,
+	}).Inc()
+}
+
+func runApp() {
+	for {
+		go performRequest()
+		time.Sleep(time.Duration(1000/RPS) * time.Millisecond)
+	}
+}
+
+func main() {
+	println("Test app run")
+	createMetrics()
+	println("Metrics were created")
+	go runApp()
+	http.Handle("/metrics", promhttp.Handler())
+	err := http.ListenAndServe(":8885", nil)
+	if err != nil {
+		panic(err.Error())
+	}
+}
